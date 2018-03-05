0868cccce2e1a87602a522b13d315e9261fabe32
fyc1007261 <fyc1007261@sjtu.edu.cn>
2018-03-05 19:20:38 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args)  {
+        int a = One.one();
+        System.out.println(a);
+    }
+
+}
+
---
+++ src/main/java/One.java
@@ -0,0 +1,5 @@
+public class One{
+    public static int one(){
+        return 1;
+    }
+}
